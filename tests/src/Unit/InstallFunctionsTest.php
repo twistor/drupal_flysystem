268971b658d428deb28ada56f3c4fad215cc441b
Chris Leppanen <chris.leppanen@gmail.com>
2015-06-11 19:23:36 -0700

    Add tests for module functions and install functions.

---
+++ tests/src/Unit/InstallFunctionsTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\flysystem\Unit\InstallFunctionsTest.
+ */
+
+namespace Drupal\Tests\flysystem\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Tests flysystem.install functions.
+ *
+ * @group flysystem
+ */
+class InstallFunctionsTest extends UnitTestCase {
+
+  protected $factory;
+
+  public function setUp() {
+    parent::setUp();
+    require_once dirname(dirname(dirname(__DIR__))) . '/flysystem.install';
+
+    $this->factory = $this->prophesize('Drupal\flysystem\FlysystemFactory');
+
+    $container = new ContainerBuilder();
+    $container->set('flysystem_factory', $this->factory->reveal());
+    $container->set('string_translation', $this->getStringTranslationStub());
+
+    \Drupal::setContainer($container);
+  }
+
+  public function testFlysystemRequirements() {
+    if (!defined('REQUIREMENT_ERROR')) {
+      define('REQUIREMENT_ERROR', 2);
+    }
+
+    $return = flysystem_requirements('install');
+    $this->assertSame(0, count($return));
+
+    $this->factory->ensure()->willReturn([
+      'testscheme' => [[
+        'message' => 'Test message',
+        'context' => [],
+      ]],
+    ]);
+
+    $return = flysystem_requirements('runtime');
+    $this->assertSame(1, count($return));
+    $this->assertSame('Test message', $return['flysystem:testscheme']['description']);
+  }
+
+  public function testFlysystemInstall() {
+    $this->factory->ensure()->shouldBeCalled();
+    flysystem_install();
+  }
+
+}
---
+++ tests/src/Unit/ModuleFunctionsTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\flysystem\Unit\ModuleFunctionsTest.
+ */
+
+namespace Drupal\Tests\flysystem\Unit;
+
+use Drupal\Core\Site\Settings;
+use Drupal\Tests\UnitTestCase;
+use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * Tests module functions.
+ *
+ * @group flysystem
+ */
+class ModuleFunctionsTest extends UnitTestCase {
+
+  protected $factory;
+
+  public function setUp() {
+    parent::setUp();
+    vfsStream::setup('module_file');
+
+    require_once dirname(dirname(dirname(__DIR__))) . '/flysystem.module';
+
+    $this->factory = $this->prophesize('Drupal\flysystem\FlysystemFactory');
+
+    $file_system_helper = $this->prophesize('Drupal\Core\File\FileSystemInterface');
+    $file_system_helper->uriScheme(Argument::type('string'))->will(function ($uri) {
+      list($scheme) = explode('://', $uri[0]);
+      return $scheme;
+    });
+
+    $guesser = $this->prophesize('Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesserInterface');
+    $guesser->guess(Argument::type('string'))->willReturn('txt/flysystem');
+
+    $container = new ContainerBuilder();
+    $container->set('file_system', $file_system_helper->reveal());
+    $container->set('flysystem_factory', $this->factory->reveal());
+    $container->set('file.mime_type.guesser.extension', $guesser->reveal());
+
+    \Drupal::setContainer($container);
+  }
+
+  public function testFlysystemCron() {
+    $this->factory->ensure()->shouldBeCalled();
+    flysystem_cron();
+  }
+
+  public function testFlysystemRebuild() {
+    $this->factory->ensure()->shouldBeCalled();
+    flysystem_rebuild();
+  }
+
+  public function testFlysystemFileDownload() {
+    new Settings(['flysystem' => []]);
+
+    $this->assertNull(flysystem_file_download('vfs://module_file/file.txt'));
+
+    new Settings(['flysystem' => ['vfs' => ['driver' => 'testdriver']]]);
+
+    file_put_contents('vfs://module_file/file.txt', '1234');
+
+    $return = flysystem_file_download('vfs://module_file/file.txt');
+
+    $this->assertSame(2, count($return));
+    $this->assertSame('txt/flysystem', $return['Content-Type']);
+    $this->assertSame(4, $return['Content-Length']);
+
+  }
+
+}
